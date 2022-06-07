97e33ea7ebc587e306516b82c1c9ad5faf1cb62c
ibraheem ibraheem <iibraheem2001@gmail.com>
2022-06-06 20:01:08 -0700

    worked on liked list code challenge

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -3,10 +3,36 @@ class LinkedList:
     Put docstring here
     """
 
-    def __init__(self):
+    def __init__(self, head=none):
         # initialization here
-        pass
+        self.head = head
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        new_node =Node(value, self.head)
+        self.head = new_node
+
+    def includes(self, value):
+        current = self.head
+        does_include = False
+        while current:
+            if current.value == value:
+                does_include = True
+            current = current.next
+        return does_include
+
+    def to_string(self):
+        current = self.head
+        s = ""
+        while current:
+            s += "{" + str(current.value) + "} -> "
+            current = current.next
+        s += "NULL"
+        return s
+
+    def __str__(self):
+        current = self.head
+        s = ""
+        while current:
+            s += "{ " + 
+    
+
